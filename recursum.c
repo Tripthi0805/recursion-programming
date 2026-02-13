17fca1306157a8242dd5e78a299e6faabea9ae6a
Tripthi0805 <rangdhaltripthi@gmail.com>
2026-02-13 20:25:03 +0530

    Add files via upload

---
+++ recursum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int add(int i,int n,int sum)
+{
+    if(i>n)
+    {
+        return sum;
+    }
+    sum=sum+i;
+    add(i+1,n,sum);
+}
+int main() {
+    int m,n;
+    int sum=0;
+    printf("Enter the start and range value:");
+    scanf("%d %d",&m,&n);
+    int sum1=add(m,n,0);
+    printf("sum1=%d\n",sum1);
+    return 0;
+}
