fc618edfb65c4490c8f947c2533a26a1718d6e8d
Tripthi0805 <rangdhaltripthi@gmail.com>
2026-02-14 23:34:44 +0530

    Update whilerecur.c

--- whilerecur.c
+++ whilerecur.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 void digit(int a)
 {
-    if(a==10)
+    if(a==0)
     {
         return;
     }
-    int digit=a%10;
-    printf("%d\n",digit);
+    int digit1=a%10;
+    printf("%d\n",digit1);
     a=a/10;
-    digit (a);
+    digit(a);
 }
 int main() {
     int a;
     printf("Enter the value:");
     scanf("%d",&a);
-    digit(int a);
-    printf("digit",a);
+    digit(a);
     return 0;
+
 }
